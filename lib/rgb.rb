4105529a4f6f0cbf30f1ca3d0170073bdb2c7b93
Tsubasaman0 <57704006+Tsubasaman0@users.noreply.github.com>
2020-01-22 00:50:17 +0900

    refactaring def ints to rgb

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -11,8 +11,8 @@ end
 #リファクタリングする。要はコードの簡略化。
 
 def to_ints(hex)
-    r, g, b = hex[1..2], hex[3..4], hex[5..6]
-    [r, g, b].map do |s|
-        s.hex
-    end
+    hex.scan(/\w\w/).map(&:hex)
 end
+
+#コードを書いたら満足せずにどこかリファクタリングできないか研究することを心がける
+
